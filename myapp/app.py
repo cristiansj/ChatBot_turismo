0b92a8274fcfc4043f319885d7e952139b678d41
cristian Solarte <cristian.solartej@uqvirtual.edu.co>
2023-05-27 13:19:45 -0500

    Se publico el proyecto de chatBot de turismo para IA

---
+++ myapp/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        with open("files/usuarios.json") as file:
+            usuarios = json.load(file)
+
+        cedula = request.form['cedula']
+        if cedula in usuarios:
+            return 'Bienvenido de nuevo!'
+        else:
+            return render_template('registro.html')
+
+    return render_template('login.html')
+
+@app.route('/registro.html', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        with open("files/usuarios.json") as file:
+            usuarios = json.load(file)
+
+        cedula = request.form['cedula']
+        if cedula in usuarios:
+            return 'Bienvenido de nuevo!'
+        else:
+            nombre = request.form['nombre']
+            edad = int(request.form['edad'])
+            pais = request.form['pais']
+            pais = pais.lower()
+            ciudad = request.form['ciudad']
+            ciudad = ciudad.lower()
+            genero = request.form['genero']
+            genero = genero.lower()
+            usuarios[cedula] = {"nombre": nombre,"edad":edad,"pais":pais,"ciudad":ciudad, "genero": genero}
+            with open("files/usuarios.json", "w") as file:
+                json.dump(usuarios, file)
+            return 'Registro completo!'
+
+    return render_template('registro.html')
+
+if __name__ == '__main__':
+    app.run(port=7000)
